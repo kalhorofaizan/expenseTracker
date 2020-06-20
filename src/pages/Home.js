836ad69a2bda124f5a14492e993f9bfba2ae1d74
kalhorofaizan <kalhorofaizan@gmail.com>
2020-06-20 14:07:53 +0500

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React, {Component,useReducer} from 'react';
+import Main from "../components/main";
+
+
+class Home extends Component {
+
+
+
+    render() {
+
+        return (
+            <div>
+
+            <Main/>
+            </div>
+        );
+    }
+}
+
+export default Home;
